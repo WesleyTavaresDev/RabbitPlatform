fb0cb3f97a0d14d4ef3cad43449b9a420699e606
Wesley <wesleyTavaresDev@outlook.com>
2021-12-31 18:27:00 -0300

    !-- Plans changed --!
    !--
    Removed:
       old player sprite ;
    
    Added:
       (Mask Dude -Pixel adventure pack) sprites
    --!
    
    Player movement ready in a basic context

--- RabbitPlatform/Assets/2.Scripts/Player/PlayerMovement.cs
+++ RabbitPlatform/Assets/2.Scripts/Player/PlayerMovement.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float horizontalSpeed;
+
+    private bool isFacingLeft;
+
+    Rigidbody2D rb;
+    
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        Move();
+    }
+
+    void Move()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(horizontalInput * Time.fixedDeltaTime * horizontalSpeed, rb.velocity.y);
+    
+        Flip(horizontalInput);
+    }
+
+    void Flip(float horizontalInput)
+    {
+        if(horizontalInput == 0)
+        {
+            return;
+        }
         
+        isFacingLeft = !isFacingLeft;
+
+        Vector2 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+        
+    
+        if(horizontalInput > 0)
+        {
+            if(isFacingLeft)
+                Flip(horizontalInput);
+        }
+
+        else
+        {
+            if(!isFacingLeft)
+                Flip(horizontalInput);
+        }
     }
 }
