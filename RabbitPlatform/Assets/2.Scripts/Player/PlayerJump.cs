f1b110b20f884b1bc366eb7cf5a89effbd0d97c5
Wesley <wesleyTavaresDev@outlook.com>
2022-01-01 21:23:49 -0300

    Player Jump
    Basic jump added to my player

---
+++ RabbitPlatform/Assets/2.Scripts/Player/PlayerJump.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerJump : MonoBehaviour
+{
+    enum JUMP_STATES { Idle, Jumping };
+    [SerializeField] JUMP_STATES jumpStates;
+    [SerializeField] private float jumpForce;
+
+    Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Jump();
+    }
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump") &&  jumpStates == JUMP_STATES.Idle)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, Time.fixedDeltaTime * jumpForce);
+            jumpStates = JUMP_STATES.Jumping;
+        }
+    }
+}
