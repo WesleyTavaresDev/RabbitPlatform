ac1a06296e703c3f26bacf408537805382b78684
Wesley <wesleyTavaresDev@outlook.com>
2022-01-01 23:09:03 -0300

    Player Jump Animation
      IsJumping => Return true if
        jumpStates are equal to
        JUMP_STATES.JUMPING;
     A anim method that receives
       IsJumping method as argument ;
     Other method that receives rb.velocity.y as argument.

--- RabbitPlatform/Assets/2.Scripts/Player/PlayerJump.cs
+++ RabbitPlatform/Assets/2.Scripts/Player/PlayerJump.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(Animator))]
 public class PlayerJump : MonoBehaviour
 {
     enum JUMP_STATES { Idle, Jumping };
@@ -13,30 +13,43 @@ public class PlayerJump : MonoBehaviour
     [SerializeField] private const int GROUND_LAYER = 8;
 
     Rigidbody2D rb;
-
+    Animator anim;
+    
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
         Jump();
+        JumpAnimation();
+
         GroundCheck();
     }
 
     void Jump()
     {
-        if(Input.GetButtonDown("Jump") &&  jumpStates == JUMP_STATES.Idle)
+        if(Input.GetButtonDown("Jump") &&  !IsJumping())
         {
             rb.velocity = new Vector2(rb.velocity.x, Time.fixedDeltaTime * jumpForce);
         }
+    }
+
+    void JumpAnimation()
+    {
+        anim.SetBool("Jumping", IsJumping());
+        anim.SetFloat("Jump", rb.velocity.y);
     }
 
     void GroundCheck()
     {
         RaycastHit2D ray = Physics2D.Raycast(rb.worldCenterOfMass, Vector2.down, checkDistance, 1 << GROUND_LAYER);
         Debug.DrawRay(rb.worldCenterOfMass, Vector2.down * checkDistance, Color.blue);
+
         jumpStates = ray.collider != null ? JUMP_STATES.Idle : JUMP_STATES.Jumping;
     }
+
+    bool IsJumping() => jumpStates == JUMP_STATES.Jumping;
 }
