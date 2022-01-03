cd06e1a46178e533bb741ba9934a48e52ddf43f0
Wesley <wesleyTavaresDev@outlook.com>
2022-01-03 01:33:01 -0300

    CollectableItem
    The player, on colliding, collect the item

---
+++ RabbitPlatform/Assets/2.Scripts/CollectableItem/CollectableItem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableItem : MonoBehaviour, ICollectable
+{
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+            Collected();
+    }
+
+    public void Collected()
+    {
+        Destroy(this.gameObject, 0.5f);
+    }
+}
