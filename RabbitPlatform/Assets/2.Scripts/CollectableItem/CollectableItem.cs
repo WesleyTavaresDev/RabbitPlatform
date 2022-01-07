f5861fadcd2e4d2496433cef817e5168d3bd3ca4
Wesley <wesleyTavaresDev@outlook.com>
2022-01-07 00:37:53 -0300

    StrawberryPoints
    A System that add points for player and send a feedback changing the number of a text in scene

--- RabbitPlatform/Assets/2.Scripts/CollectableItem/CollectableItem.cs
+++ RabbitPlatform/Assets/2.Scripts/CollectableItem/CollectableItem.cs
@@ -5,23 +5,32 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class CollectableItem : MonoBehaviour, ICollectable
 {
+    [SerializeField] private int itemPoints;
+
+    public delegate void OnCollect(int point);
+    public static event OnCollect onCollected;
+
     Animator anim;
 
-    void Start()
-    {
-        anim = GetComponent<Animator>();
-    }
-
+    void Start() => anim = GetComponent<Animator>();
+    
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.CompareTag("Player"))
+        {
+            onCollected?.Invoke(itemPoints);
             Collected();
+        }
     }
 
-    public void Collected()
-    {
-        anim.SetTrigger("Collected");
-    }
+    public void Collected() => anim.SetTrigger("Collected");
+    
+    void DestroyAfterCollected() => Destroy(this.gameObject); 
 
-    void DestroyAfterCollected() => Destroy(this.gameObject);
+    /**
+    *! f/void OnEnable() => onCollected += Collected;
+    *!void OnDisable() => onCollected -= Collected;
+    */
+
 }
+    
---
+++ RabbitPlatform/Assets/2.Scripts/StrawberryPoints/StrawberryPoints.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StrawberryPoints : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI pointText;
+    int points;
+
+    void UpdateScore(int point)
+    {
+        points += point;
+        pointText.text = 'x' + points.ToString();
+    }
+
+    void OnEnable() => CollectableItem.onCollected += UpdateScore;
+
+    void OnDisable() => CollectableItem.onCollected -= UpdateScore;
+}
+
