a79e9ec239d467ff2c0b7eb9c5b25808fe3abb77
Wesley <wesleyTavaresDev@outlook.com>
2022-01-19 17:58:14 -0300

    Player Life change
    Invoke a delegate when the player  collide with spikes

--- RabbitPlatform/Assets/2.Scripts/Player/PlayerLife.cs
+++ RabbitPlatform/Assets/2.Scripts/Player/PlayerLife.cs
@@ -5,9 +5,18 @@ using DG.Tweening;
 
 public class PlayerLife : MonoBehaviour
 {
+    public delegate void OnPlayerDeath();
+    public static event OnPlayerDeath playerDeath;
+
     Rigidbody2D rb;
     Animator anim;
     Collider2D collider;
+
+    void OnEnable()
+    {
+        playerDeath += OnDie;
+    }
+
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -18,12 +27,14 @@ public class PlayerLife : MonoBehaviour
     void OnCollisionEnter2D(Collision2D other)
     {
         if(other.gameObject.CompareTag("Spikes"))
-        {
-            anim.SetBool("Hit", true);
-            DisableCollider();
-        }
+            playerDeath?.Invoke();
+    }
+
+    void OnDie()
+    {
+        anim.SetBool("Hit", true);
+        DisableCollider();
     }
 
     void DisableCollider() => collider.enabled = false;
-
 }
