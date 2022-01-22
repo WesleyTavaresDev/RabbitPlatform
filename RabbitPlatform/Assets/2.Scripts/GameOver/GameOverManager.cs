f20a3342adfd6dd9f12336e7baeabbd1dfaa1a27
Wesley <wesleyTavaresDev@outlook.com>
2022-01-22 16:08:05 -0300

    GameOver Active when player die

---
+++ RabbitPlatform/Assets/2.Scripts/GameOver/GameOverManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverManager : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup gameOverComponents;
+    [SerializeField] private Canvas gameOverCanvas;
+    [SerializeField] private GraphicRaycaster gameOverRaycaster;
+    void OnEnable() => PlayerLife.playerDeath += ActiveGameOver;
+
+    void ActiveGameOver()
+    {
+        gameOverCanvas.enabled = true;
+        gameOverRaycaster.enabled = true;
+        gameOverComponents.enabled = true;
+    } 
+
+    void OnDisable() => PlayerLife.playerDeath -= ActiveGameOver;
+}
